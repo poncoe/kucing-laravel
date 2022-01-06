829dd5403160733ccdabd35bbabad0884abfd5f9
poncoe <poncoe4mac@gmail.com>
2022-01-06 07:00:34 +0700

    added content "Tentang Kami"

--- resources/views/tentang.blade.php
+++ resources/views/tentang.blade.php
@@ -1,50 +1,92 @@
 <!doctype html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Web Icon -->
-    <link rel="icon" href="https://avatars0.githubusercontent.com/u/11747628?s=460&v=4" type="image/icon type">
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    <!-- Title Web -->
-    <title>Animolz : Tentang Aplikasi</title>
+  <!-- Web Icon -->
+  <link rel="icon" href="https://avatars0.githubusercontent.com/u/11747628?s=460&v=4" type="image/icon type">
 
-    <!-- Style -->
+  <!-- Title Web -->
+  <title>Animolz : Tentang Aplikasi</title>
 
-    <style>
-        div {
-        padding: 0px 0px 0px 20px;
-        }
-    </style>
+  <!-- Style -->
 
-    <!-- Style -->
+  <style>
+    div {
+      padding: 0px 0px 0px 20px;
+    }
+  </style>
 
-  </head>
-  <body>
+  <!-- Style -->
 
-    <div class="container">
-        <h2 class="alert alert-primary text-center mt-5">Yoi Mamen</h2>
-        <form action="tambahArtikel.php" method="POST" enctype="multipart/form-data"> 
-        <div class="form-group">
+</head>
 
-            </div>
+<body>
 
-            <div>
+  <div class="container">
+    <h2 class="alert alert-primary text-center mt-5">Tim Animolz - Yoi Mamen</h2>
+
+    <!-- Artikel -->
+    <section id="artikel ">
+      <div class="container ">
+        <div class="row text-center mb-3 ">
+          <div class="col ">
+            <br>
             <br><br>
-              <p class="fs-5">Kembali ke <a href="http://localhost:8000/" class="pe-auto">Halaman Utama</a> ?</p>
-              <br><br>
-    </div>
-        </div>
-        </form>
-    </div>
+          </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
-  </body>
+          <!-- Tim -->
+          <div class="container marketing ">
+
+            <!-- Three columns of text below the carousel -->
+            <div class="row ">
+              <div class="col-lg-4 ">
+                <img src="/img/hipsi.jpeg " class="img-thumbnail rounded-circle " width="200 " height="140 " alt="hipsi " focusable="false ">
+                <br><br>
+                <h2>Hifdzi Hisan</h2>
+                <p>Ngetes-Ngetes & Ngoding Tampilan Layar Biar Mantep</p>
+              </div>
+              <!-- /.col-lg-4 -->
+              <div class="col-lg-4 ">
+                <img src="/img/tiara.jpeg" class="img-thumbnail rounded-circle " width="240 " height="140 " alt="tiara " focusable="false ">
+                <br><br>
+                <h2>Tiara Siahaan</h2>
+                <p>Ngetes-Ngetes juga Sekalian Bikin Dokumen Website</p>
+              </div>
+              <!-- /.col-lg-4 -->
+              <div class="col-lg-4 ">
+                <img src="/img/coedotz.png " class="img-thumbnail rounded-circle " width="210 " height="140 " alt="poncoe " focusable="false ">
+                <br><br>
+                <h2>Arief Wardhana</h2>
+                <p>Ngoding Belakang Layar</p>
+              </div>
+            </div>
+            <!-- /.row -->
+            <!-- End Tim -->
+
+          </div>
+        </div>
+    </section>
+    <!-- End Artikel -->
+
+    <div>
+      <br><br>
+      <p class="fs-5">Kembali ke <a href="http://localhost:8000/" class="pe-auto">Halaman Utama</a> ?</p>
+      <br><br>
+    </div>
+  </div>
+  </form>
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+</body>
+
 </html>
