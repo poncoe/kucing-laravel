68eff32f9a24f963c0cdfa676ba737df678e1de4
poncoe <poncoe4mac@gmail.com>
2022-01-08 13:37:18 +0700

    fix style btn hidding

--- resources/views/jeniskucing/edit.blade.php
+++ resources/views/jeniskucing/edit.blade.php
@@ -53,7 +53,7 @@
         </div>
 
         <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button class="btnfix" type="submit" class="btn btn-primary">Submit</button>
         </div>
     </div>
 
